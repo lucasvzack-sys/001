abf6f945f7d07631d1f287aa4921b80db70061b1
lucasvzack-sys <lucasvzack@gmail.com>
2026-04-14 16:45:00 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ export default function App() {
               </motion.div>
             } />
 
-            <Route path="/calculai" element={
+            <Route path="/calculai/:calcId?" element={
               <motion.div
                 initial={{ opacity: 0, x: 20 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -20 }} transition={{ duration: 0.3 }}
               >
